7449f2b1f6ad94246dc9091c300897562ff2389a
Mario Menjívar <mariomenjr@gmail.com>
2021-02-15 22:30:07 -0800

    Move PriceCollection to configs

--- model/config.js
+++ model/config.js
@@ -6,9 +6,30 @@ const Columns = {
   Close: 4,
   Volume: 5,
   MarketCap: 6,
-  Weekday: 7
+  Weekday: 7,
 };
 
+const PriceCollection = [
+  `data/bitcoin_cash_price.csv`,
+  `data/bitcoin_price.csv`,
+  `data/bitconnect_price.csv`,
+  `data/dash_price.csv`,
+  `data/ethereum_classic_price.csv`,
+  `data/ethereum_price.csv`,
+  `data/iota_price.csv`,
+  `data/litecoin_price.csv`,
+  `data/monero_price.csv`,
+  `data/nem_price.csv`,
+  `data/neo_price.csv`,
+  `data/numeraire_price.csv`,
+  `data/omisego_price.csv`,
+  `data/qtum_price.csv`,
+  `data/ripple_price.csv`,
+  `data/stratis_price.csv`,
+  `data/waves_price.csv`,
+];
+
 module.exports = {
-  Columns
+  Columns,
+  PriceCollection
 };
--- model/index.js
+++ model/index.js
@@ -3,30 +3,11 @@ const tf = require("@tensorflow/tfjs-node");
 const fileReader = require("./utils/fileReader");
 const processData = require("./controllers/processData");
 const makeTensors = require("./controllers/makeTensors");
+const { PriceCollection } = require("./config");
 
 let isPromiseResolved = false;
 
-Promise.all(
-  [
-    `data/bitcoin_cash_price.csv`,
-    `data/bitcoin_price.csv`,
-    `data/bitconnect_price.csv`,
-    `data/dash_price.csv`,
-    `data/ethereum_classic_price.csv`,
-    `data/ethereum_price.csv`,
-    `data/iota_price.csv`,
-    `data/litecoin_price.csv`,
-    `data/monero_price.csv`,
-    `data/nem_price.csv`,
-    `data/neo_price.csv`,
-    `data/numeraire_price.csv`,
-    `data/omisego_price.csv`,
-    `data/qtum_price.csv`,
-    `data/ripple_price.csv`,
-    `data/stratis_price.csv`,
-    `data/waves_price.csv`,
-  ].map((filePath) => fileReader(filePath))
-)
+Promise.all(PriceCollection.map((filePath) => fileReader(filePath)))
   .then(processData())
   .then(makeTensors())
   .catch((e) => console.error({ e }))
