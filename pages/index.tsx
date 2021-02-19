ab85772e72d57406daecfeafc541d384f143cdc9
Mario Menjívar <mariomenjr@gmail.com>
2021-02-19 00:25:17 -0800

    wip: Consume model through web app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
-function Index() {
-  return <div>Welcome to Next.js!</div>;
+import { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { model, sample } = context.req.wgu;
+  const prediction: any = await model.predict(sample);
+  return { props: { prediction: await prediction.array() } };
+};
+
+function Index(props) {
+  return (
+    <div>
+      Welcome to Next.js!
+      <br />
+      <pre>{JSON.stringify(props)}</pre>
+    </div>
+  );
 }
 
 export default Index;
