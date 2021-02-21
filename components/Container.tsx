93e9229596131d0c2f037b6bd1f3294d2f0d8c00
Mario Menjívar <mariomenjr@gmail.com>
2021-02-20 18:37:49 -0800

    wip: Make it work on phone screens

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,7 +1,7 @@
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
     <div
-      className={`container w-screen h-screen flex justify-center items-center overflow-hidden`}
+      className={`container w-screen h-screen flex justify-center items-center overflow-hidden bg-gray-300 bg-opacity-0`}
     >
       {children}
     </div>
--- components/Main.tsx
+++ components/Main.tsx
@@ -2,30 +2,36 @@ export default function Main() {
   const currentCurrency = `Bitcoin`;
   const predictedDay = `Wednesday`;
   return (
-    <div className={`flex flex-col`}>
-      <h4>
-        It's better to buy{" "}
+    <div className={`flex flex-col px-16`}>
+      <h4 className={`py-3`}>
+        <span>It's better to buy </span>
         <span className={`italic font-semibold underline`}>
           {currentCurrency}
-        </span>{" "}
-        on a...
+        </span>
+        <span> on a...</span>
       </h4>
       <h1
-        className={`font-sans font-semibold text-8xl tracking-wider text-yellow-500 text-center py-3`}
+        className={`font-sans font-semibold text-4xl sm:text-8xl tracking-wide text-blue-500 text-center py-3`}
       >
         {predictedDay}
       </h1>
       <div className={`container w-full py-3`}>
-        <div className={`grid grid-cols-3 gap-2`}>
+        <div className={`grid grid-cols-1 sm:grid-cols-3  gap-2`}>
           <select
-            className={`block px-3 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline sm:text-sm md:text-md`}
+            className={`block px-3 py-2 rounded leading-tight focus:outline-none sm:text-sm md:text-md`}
           >
             <option value="btc">Bitcoin</option>
             <option value="eth">Etherum</option>
             <option value="dgc">Dogecoin</option>
           </select>
-          <input type="text" className={`block px-3 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline sm:text-sm md:text-md`} />
-          <input type="text" className={`block px-3 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline sm:text-sm md:text-md`} />
+          <input
+            type="text"
+            className={`block px-3 py-2 rounded leading-tight focus:outline-none sm:text-sm md:text-md`}
+          />
+          <input
+            type="text"
+            className={`block px-3 py-2 rounded leading-tight focus:outline-none sm:text-sm md:text-md`}
+          />
         </div>
       </div>
     </div>
