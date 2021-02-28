a5c3a25e0e24a3d2efb47ca97c2e00d9c2ab096b
Mario Menjívar <mariomenjr@gmail.com>
2021-02-28 00:39:16 -0800

    Handle currentCurrency.label when currentCurrency is null

--- components/DecorativeLegend.tsx
+++ components/DecorativeLegend.tsx
@@ -2,7 +2,7 @@ export default function DecorativeLegend({ currentCurrency }) {
   return <h4 className={`py-3`}>
     <span>It's better to buy </span>
     <span className={`italic font-semibold underline`}>
-      {currentCurrency.label}
+      {currentCurrency?.label ?? `...`}
     </span>
     <span> on a...</span>
   </h4>;
